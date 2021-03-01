fd99e95644147914a5cbae98ced44db91254e6fe
linyw <linyw@si-tech.com.cn>
2021-03-01 14:12:03 +0800

    mm

---
+++ src/test/java/com/linyw/droolsdemo/DroolsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linyw.droolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
